6ab18a08e118e9f700b66af67ce8ff17b980e958
Euginy Azaryonok <jonarizzz@mbp-euginy.iba>
2019-05-03 16:23:03 +0300

    Setting up basic React project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './Header'
+import Content from './Content'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Content/>
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Content extends Component {
+    render(){
+        return(
+            <div>
+                Content component
+            </div>
+        )
+    }
+}
+
+export default Content
