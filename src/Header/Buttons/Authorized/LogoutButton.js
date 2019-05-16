c34462c2dcecadc2c6dc8f3cc9a6eba139f1eeee
jonarizzz <jonarizzz@inbox.ru>
2019-05-16 18:15:50 +0300

    Some minor styles refactoring

--- src/Header/Buttons/Authorized/LogoutButton.js
+++ src/Header/Buttons/Authorized/LogoutButton.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import { Button } from "@material-ui/core";
+import { Button, withStyles } from "@material-ui/core";
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    }
+    
+})
 
 class LogoutButton extends Component {
     render(){
+        const {classes} = this.props;
         return(
-            <Button color="inherit" variant = 'outlined'>Login</Button>
+            <Button color = 'secondary' variant = 'contained' className = {classes.button}> Logout </Button>
         )
     }
 }
 
-export default LogoutButton
+export default withStyles(styles) (LogoutButton)
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,6 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import LoginButton from './Buttons/Unauthorized/LoginButton'
+import LogoutButton from './Buttons/Authorized/LogoutButton';
 
 const styles = {
     root: {
@@ -25,6 +26,7 @@ class Header extends Component {
                             Insurance agent workstation
                         </Typography>
                         <LoginButton/>
+                        <LogoutButton/>
                     </Toolbar>
                 </AppBar>
             </div>
